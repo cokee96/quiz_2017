2847028c35c6b06cf5bace28307e5d7198f5e15e
cokee96 <alvaro.alonso.gonzalez@alumnos.upm.es>
2017-05-24 16:30:12 +0200

    Primeros Cambios

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.render('index');
+});
+
+/* GET ayuda page. */
+router.get('/help', function(req, res, next) {
+    res.render('help');
 });
 
 // Pagina de creditos
